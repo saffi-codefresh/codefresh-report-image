be010b830cb52465f54bfdba1f68ed053b7668f7
Saffi <saffi.hartal@codefresh.io>
2022-07-14 16:27:02 +0300

    bump

--- .deploy/pipelines/scripts/ci/bumpCommit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-release-it --patch --ci
-head package.json
-cat service.yaml
-.deploy/pipelines/scripts/ci/name-version.sh > ./service.yaml
-git commit -m "bump" && git push
--- .deploy/pipelines/scripts/promote/release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-release-it --patch --ci
-git tag -f latest
-git push --tag --force
